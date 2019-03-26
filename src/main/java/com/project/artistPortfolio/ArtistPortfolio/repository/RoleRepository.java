acc31f82df853e5f3d149b8b8b216e0c3e7a1f5c
anjukmf <anjuk@mindfiresolutions.com>
2019-03-26 11:35:02 +0530

    added method to get role by role name

--- src/main/java/com/project/artistPortfolio/ArtistPortfolio/repository/RoleRepository.java
+++ src/main/java/com/project/artistPortfolio/ArtistPortfolio/repository/RoleRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.artistPortfolio.ArtistPortfolio.model.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer>{
+	
+	Role findByRole(String Role);
 
 }
