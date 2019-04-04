cfe626d57d24640113ed7e59f7d2c0ba25cd2a3f
anjukmf <anjuk@mindfiresolutions.com>
2019-04-04 15:23:47 +0530

    changed method return type

--- src/main/java/com/project/artistPortfolio/ArtistPortfolio/service/MediaService.java
+++ src/main/java/com/project/artistPortfolio/ArtistPortfolio/service/MediaService.java
@@ -5,7 +5,7 @@ import com.project.artistPortfolio.ArtistPortfolio.model.Media;
 public interface MediaService {
 	
 	Media getMediaById(int id);
-	void createMedia(Media media);
+	int createMedia(Media media);
 	void updateMedia(int id,Media media);
 	String deleteMediaById(int id);
 
