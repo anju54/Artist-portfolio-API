7f8a86ff5019ebd6e6806cf88eb51b94525cd541
anjukmf <anjuk@mindfiresolutions.com>
2019-04-10 22:33:49 +0530

    added method getMediaByFileName

--- src/main/java/com/project/artistPortfolio/ArtistPortfolio/repository/MediaRepository.java
+++ src/main/java/com/project/artistPortfolio/ArtistPortfolio/repository/MediaRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.artistPortfolio.ArtistPortfolio.model.Media;
 
 public interface MediaRepository extends JpaRepository<Media, Integer>{
+	
+	Media getMediaByFileName(String fileName);
 
 }
